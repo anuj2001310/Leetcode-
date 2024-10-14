cb350166a5715129527678530ee57daf24ff4d09
anuj2001310 <thakuranujbhati310@gmail.com>
2024-10-14 14:56:27 +0530

    Time: 653 ms (96.83%), Space: 31.2 MB (78.48%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.py
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.py
@@ -1,14 +1,9 @@
-class Solution(object):
-    def maxKelements(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
         pq = [-x for x in nums]
         heapify(pq)
-        ans = 0 
-        for i in range(k): 
-            ans -= pq[0] 
+        ans = 0
+        for i in range(k):
+            ans -= pq[0]
             heapreplace(pq, pq[0] // 3)
         return ans
