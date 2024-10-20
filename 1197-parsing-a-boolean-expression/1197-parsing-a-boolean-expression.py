37b12a16594a3194559a61f47d7ec36c82fae4a4
anuj2001310 <thakuranujbhati310@gmail.com>
2024-10-20 14:43:34 +0530

    Time: 35 ms (82.61%), Space: 12 MB (52.17%) - LeetHub

---
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def parseBoolExpr(self, expression):
+        """
+        :type expression: str
+        :rtype: bool
+        """
+        stack = []
+        for curr in expression:
+            if curr == ',' or curr == '(':
+                continue
+            if curr == 't' or curr == 'f' or curr == '!' or curr == '&' or curr == '|':
+                stack.append(curr)
+            
+            elif curr == ')':
+                has_true, has_false = False, False
+                while stack[-1] != '!' and stack[-1] != '&' and stack[-1] != '|':
+                    top_val = stack[-1]
+                    stack.pop()
+                    if top_val == 't':
+                        has_true = True
+                    if top_val == 'f':
+                        has_false = True
+                
+                op = stack[-1]
+                stack.pop()
+                if op == '!':
+                    stack.append('f' if has_true else 't')
+                elif op == '&':
+                    stack.append('f' if has_false else 't')
+                else:
+                    stack.append('t' if has_true else 'f')
+        
+        return stack[-1] == 't' 
