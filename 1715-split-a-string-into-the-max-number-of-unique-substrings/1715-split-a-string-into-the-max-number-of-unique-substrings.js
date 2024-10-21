40a32fb12ccfcf03a665e6e6cc246ca4e42d6c62
anuj2001310 <thakuranujbhati310@gmail.com>
2024-10-21 22:26:07 +0530

    Time: 33 ms (100%), Space: 54.9 MB (33.33%) - LeetHub

---
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxUniqueSplit = function (s) {
+    const set = new Set();
+
+    const traverse = (s, start, set) => {
+        if (start === s.length)
+            return 0;
+        let max = 0;
+        for (let i = start + 1; i <= s.length; i++) {
+            let substr = s.substring(start, i);
+            if (set.has(substr))
+                continue;
+            if (max >= set.size + s.length - start)
+                continue;
+    
+            set.add(substr);
+            max = Math.max(max, 1 + traverse(s, i, set))
+            set.delete(substr);
+        }
+        return max;
+    }
+
+    return traverse(s, 0, set)
+};
