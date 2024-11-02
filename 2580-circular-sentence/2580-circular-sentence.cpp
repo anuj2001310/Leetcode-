15ed3800ab8011684d8cffafdfceb748da0093ca
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-02 17:20:30 +0530

    Time: 0 ms (100%), Space: 8.3 MB (59.78%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        /*
-        vector<string> help;
-        string temp = "";
-        for (int i = 0; i < sentence.length(); ++i) {
-            if (sentence[i] == ' ') {
-                help.push_back(temp);
-                temp = "";
-            } else
-                temp += sentence[i];
-        }
-        help.push_back(temp);
-
-        for (auto& str : help)
-            cout << str << " -> ";
-        */
         int n = sentence.length();
         if (sentence[0] != sentence[n - 1])
             return false;
