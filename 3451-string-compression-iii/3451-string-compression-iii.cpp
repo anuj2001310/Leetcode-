b75276b35de59c6accc0d3cdca306d955d4e6959
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-04 11:40:36 +0530

    Time: 33 ms (63.25%), Space: 29.8 MB (57.63%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     string compressedString(string word) {
-        int i = 0;
         string comp = "";
-        while (i < word.length()) {
+        int i = 0;
+        while(i < word.length()){
             int cnt = 1;
-            char curr = word[i];
-            while (word[i + 1] == curr and (i + 1) < word.length() and
-                   cnt < 9) {
-                cnt++;
+            char curr_char = word.at(i);
+            while((i + 1) < word.length() and word[i + 1] == curr_char and cnt < 9){
+                cnt += 1;
                 i++;
             }
-            comp += to_string(cnt) + curr;
+            comp += to_string(cnt) + curr_char;
             i++;
         }
         return comp;
