45426c0945be728f9ee90398803908e402e17c36
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-07 14:27:09 +0530

    Time: 834 ms (12.5%), Space: 26.7 MB (81.82%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.py
@@ -1,18 +1,14 @@
-class Solution(object):
-    def largestCombination(self, candidates):
-        """
-        :type candidates: List[int]
-        :rtype: int
-        """
-        freq = [0]  * 32
+class Solution:
+    def largestCombination(self, candidates: List[int]) -> int:
+        freq = [0] * 32
+
         def dp(n):
             for i in range(32):
                 m = 1 << i
                 if m & n:
                     freq[i] += 1
-            
-        
+
         for ele in candidates:
             dp(ele)
-        
+
         return max(freq)
