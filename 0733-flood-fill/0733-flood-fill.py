7886a62d81f4487cb49331a1dd32d7d0ded8b44a
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-08 13:04:53 +0530

    Time: 0 ms (100%), Space: 11.6 MB (77.3%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -13,14 +13,19 @@ class Solution(object):
 
         initial = image[sr][sc]
         ans = image
-        def dfs(r, c, image, ans):
-            ans[r][c] = color
+        q = deque()
+        q.append((sr, sc))
+        ans[sr][sc] = color
+        while q:
+            curr = q.popleft()
+            r = curr[0]
+            c = curr[1]
             for i in range(len(dirs)):
                 nr = r + dirs[i][0]
                 nc = c + dirs[i][1]
                 if nr >= 0 and nr < n and nc >= 0 and nc < m and image[nr][nc] == initial and ans[nr][nc] != color:
-                    dfs(nr, nc, image, ans)
+                    q.append((nr, nc))
+                    ans[nr][nc] = color
         
-
-        dfs(sr, sc, image, ans)
         return ans
+        
