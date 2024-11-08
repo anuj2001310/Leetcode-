3997c435c7e42dc3a47e509fb0d3d92e5c7a1030
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-08 15:12:51 +0530

    Time: 2 ms (12.5%), Space: 13.6 MB (6.49%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.c
+++ 0733-flood-fill/0733-flood-fill.c
@@ -5,18 +5,67 @@
  * caller calls free().
  */
 
- //DFS Solution in C
+// BFS Solution in C
 int dirs[][4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-void dfs(int r, int c, int** image, int** ans, int n, int m, int initial, int color) {
-    ans[r][c] = color;
-    for (int i = 0; i < 4; i++) {
-        int nr = r + dirs[i][0];
-        int nc = c + dirs[i][1];
-        if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
-            image[nr][nc] == initial && ans[nr][nc] != color)
-            dfs(nr, nc, image, ans, n, m, initial, color);
-    }
+typedef struct pair {
+    int first;
+    int second;
+} pair;
+
+pair* new_pair(int first, int second) {
+    pair* p = (pair*)malloc(sizeof(pair));
+    p->first = first;
+    p->second = second;
+    return p;
+}
+typedef struct queue {
+    struct pair** arr;
+    int capacity;
+    int front;
+    int rear;
+    int size;
+} queue;
+
+queue* createQueue(int capacity) {
+    queue* q = (queue*)malloc(sizeof(queue));
+    q->capacity = capacity;
+    q->front = 0;
+    q->rear = capacity - 1;
+    q->size = 0;
+    q->arr = (struct pair**)malloc(sizeof(struct pair*) * capacity);
+    return q;
+}
+
+bool isEmpty(queue* q) { 
+    return q->size == 0;
+}
+
+bool isFull(queue* q) { 
+    return q->size == q->capacity; 
+}
+
+void push(queue* q, struct pair* p) {
+    if (isFull(q))
+        return;
+    q->rear = (q->rear + 1) % (q->capacity);
+    q->arr[q->rear] = p;
+    q->size++;
+}
+
+struct pair* pop(queue* q) {
+    if (isEmpty(q))
+        return NULL;
+
+    struct pair* p = q->arr[q->front];
+    q->front = (q->front + 1) % (q->capacity);
+    q->size--;
+    return p;
+}
+
+void freeQueue(queue* q) {
+    free(q->arr);
+    free(q);
 }
 
 int** floodFill(int** image, int imageSize, int* imageColSize, int sr, int sc, int color, int* returnSize, int** returnColumnSizes) {
@@ -31,7 +80,25 @@ int** floodFill(int** image, int imageSize, int* imageColSize, int sr, int sc, i
             ans[i][j] = image[i][j];
     }
 
-    dfs(sr, sc, image, ans, n, m, initial, color);
+    queue* q = createQueue(n * m);
+    push(q, new_pair(sr, sc));
+    ans[sr][sc] = color;
+
+    while (!isEmpty(q)) {
+        struct pair* pr = pop(q);
+        int r = pr->first;
+        int c = pr->second;
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dirs[i][0];
+            int nc = c + dirs[i][1];
+            if (nr >= 0 && nr < n && nc >= 0 && nc < m && image[nr][nc] == initial && ans[nr][nc] != color) {
+                push(q, new_pair(nr, nc));
+                ans[nr][nc] = color;
+            }
+        }
+    }
+
+    freeQueue(q);
     *returnSize = imageSize;
     *returnColumnSizes = imageColSize;
 
