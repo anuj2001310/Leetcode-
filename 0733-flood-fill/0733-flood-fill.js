2076eccf1c4bdcab3aedbd041f3e2958859360e7
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-08 14:20:41 +0530

    Time: 5 ms (8.84%), Space: 53.9 MB (34.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -9,18 +9,28 @@ var floodFill = function (image, sr, sc, color) {
     var n = image.length
     var m = image[0].length
     var ini = image[sr][sc]
+
     const ans = image
     const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-    const dfs = (r, c, image, ans) => {
-        ans[r][c] = color
+
+    const q = [[sr, sc]]
+    ans[sr][sc] = color
+
+    while (q.length != 0) {
+        let arr = q.shift()
+        let r = arr[0]
+        let c = arr[1]
+
         for (let i = 0; i < dirs.length; ++i) {
             let nr = r + dirs[i][0]
             let nc = c + dirs[i][1]
-            if (nr >= 0 && nr < n && nc >= 0 && nc < m && image[nr][nc] === ini && ans[nr][nc] !== color)
-                dfs(nr, nc, image, ans)
+
+            if (nr >= 0 && nr < n && nc >= 0 && nc < m && image[nr][nc] === ini && ans[nr][nc] !== color) {
+                q.push([nr, nc])
+                ans[nr][nc] = color
+            }
         }
     }
-    dfs(sr, sc, image, ans)
-
+    
     return ans
 };
