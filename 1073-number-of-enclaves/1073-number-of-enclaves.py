86ce14e8b51b8225561dd2674ea5e4e2a2240f82
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-09 16:12:12 +0530

    Time: 101 ms (54.03%), Space: 35.4 MB (35.28%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -1,32 +1,24 @@
-class Solution(object):
-    def numEnclaves(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
         n = len(grid)
         m = len(grid[0])
         vis = [[False for _ in range(m)] for _ in range(n)]
-        #print(vis)
-        dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-        q = deque()
-        for i in range(n):
-            for j in range(m):
-                if i == 0 or j == 0 or i == n - 1 or j == m - 1:
-                    if grid[i][j] == 1:
-                        vis[i][j] = True
-                        q.append((i, j))
-        
-        while q:
-            l = q.popleft()
-            #print(type(l))
-            r = l[0]
-            c = l[1]
+
+        def dfs(r, c):
+            vis[r][c] = True
             for i in range(len(dirs)):
                 nr = r + dirs[i][0]
                 nc = c + dirs[i][1]
                 if nr >= 0 and nr < n and nc >= 0 and nc < m and grid[nr][nc] == 1 and not vis[nr][nc]:
-                    q.append((nr, nc))
-                    vis[nr][nc] = True
-                    
+                    dfs(nr, nc)
+        
+
+        for i in range(n):
+            for j in range(m):
+                if i == 0 or j == 0 or i == n - 1 or j == m - 1:
+                    if grid[i][j] == 1 and not vis[i][j]:
+                        dfs(i, j)
+        
         return sum(1 for i in range(n) for j in range(m) if grid[i][j] == 1 and not vis[i][j])
