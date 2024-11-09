3088cb1f315d75b3055465ba4674bdfb9103af04
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-09 15:27:17 +0530

    Time: 261 ms (8.57%), Space: 49.1 MB (10.7%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,38 +1,34 @@
 class Solution {
     vector<vector<int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    void dfs(int r, int c, vector<vector<int>>& grid, vector<vector<bool>>& vis,
+             int n, int m) {
+        vis[r][c] = true;
+        for (int i = 0; i < dir.size(); ++i) {
+            int nr = r + dir[i][0];
+            int nc = c + dir[i][1];
+            if (nr >= 0 and nr < n and nc >= 0 and nc < m and grid[nr][nc] and
+                !vis[nr][nc]) {
+                dfs(nr, nc, grid, vis, n, m);
+            }
+        }
+    }
+
 public:
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
         vector<vector<bool>> vis(n, vector<bool>(m, false));
-        queue<pair<int, int>> q;
+
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 if (i == 0 or i == n - 1 or j == 0 or j == m - 1) {
-                    if (grid[i][j]) {
-                        vis[i][j] = true;
-                        q.push({i, j});
+                    if (grid[i][j] and !vis[i][j]) {
+                        dfs(i, j, grid, vis, n, m);
                     }
                 }
             }
         }
-
-        while (!q.empty()) {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            for (int i = 0; i < dir.size(); ++i) {
-                int nr = r + dir[i][0];
-                int nc = c + dir[i][1];
-                if (nr >= 0 and nr < n and nc >= 0 and nc < m and
-                    grid[nr][nc] and !vis[nr][nc]) {
-                    q.push({nr, nc});
-                    vis[nr][nc] = true;
-                }
-            }
-        }
-
         int cnt = 0;
 
         for (int i = 0; i < n; ++i) {
@@ -41,7 +37,7 @@ public:
                     ++cnt;
             }
         }
-        
+
         return cnt;
     }
 };
