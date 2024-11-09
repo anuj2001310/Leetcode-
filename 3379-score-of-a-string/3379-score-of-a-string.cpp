dfbfb00515804adc449dec86b95171f7e47cbb16
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-09 18:50:13 +0530

    Time: 0 ms (100%), Space: 8.7 MB (27.94%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.cpp
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int scoreOfString(string s) {
+        ios_base::sync_with_stdio(false);
+        cout.tie(nullptr);
         int sum = 0;
         for (int i = s.length() - 1; i > 0; --i)
             sum += fabs(s[i] - s[i - 1]);
