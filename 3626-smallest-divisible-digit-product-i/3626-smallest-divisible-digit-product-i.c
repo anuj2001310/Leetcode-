3d9163b74972bc8c38b8f04bf5fa5e4a9b2bd1d9
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-10 11:20:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (100%) - LeetHub

---
+++ 3626-smallest-divisible-digit-product-i/3626-smallest-divisible-digit-product-i.c
@@ -0,0 +1,13 @@
+int smallestNumber(int n, int t) {
+    for (; n; ++n) {
+        int num = n;
+        int pro = 1;
+        while (num) {
+            pro *= (num % 10);
+            num /= 10;
+        }
+        if (pro % t == 0)
+            return n;
+    }
+    return -1;
+}
