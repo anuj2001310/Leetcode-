a9ccdb7b4c62ef83c166b4daab44d928cd2f4717
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-24 14:01:13 +0530

    Time: 36 ms (100%), Space: 17.7 MB (70.83%) - LeetHub

--- 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
@@ -1,17 +1,21 @@
-class Solution:
-    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        sm, cbt = 0, 1
-        mini = float('inf')
-        
-        n = len(matrix)
-        m = len(matrix[0])
-        for i in range(n):
-            for j in range(m):
-                if matrix[i][j] < 0:
-                    cbt += 1
-                sm += abs(matrix[i][j])
-                mini = min(mini, abs(matrix[i][j]))
-        
-        if cbt & 1 == 0:
-            return sm - 2 * abs(mini)
-        return sm
+class Solution(object):
+    def maxMatrixSum(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+        total_sum = 0
+        negative_count = 0 
+        minel = float('inf')
+        for i in matrix:
+            for j in i:
+                if j < 0:
+                    j *= -1
+                    negative_count += 1
+                total_sum += j
+                if j < minel:
+                    minel = j
+        if negative_count & 1 == 0:
+            return total_sum
+        else:
+            return total_sum - 2 * minel
