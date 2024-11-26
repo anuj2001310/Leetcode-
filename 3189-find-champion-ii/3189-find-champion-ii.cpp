fa92923c68f43bc7883554f5b658682a9efae319
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-26 18:37:04 +0530

    Time: 0 ms (100%), Space: 93.9 MB (66.24%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.cpp
+++ 3189-find-champion-ii/3189-find-champion-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-        vector<int> indegree(n, 0);
+        vector<int> inDeg(n, 0);
         for (int i = 0; i < edges.size(); ++i) {
             int u = edges[i][0];
             int v = edges[i][1];
-            indegree[v]++;
+            inDeg[v]++;
         }
-
         int cnt = 0, ans = -1;
-        for (int i = 0; i < indegree.size(); ++i) {
-            if (indegree[i] == 0) {
-                cnt++;
+        for (int i = 0; i < inDeg.size(); ++i) {
+            if (inDeg[i] == 0) {
+                ++cnt;
                 ans = i;
             }
         }
