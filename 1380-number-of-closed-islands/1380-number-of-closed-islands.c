69926374809811ce2198c035996c75d490c20e8f
anuj2001310 <thakuranujbhati310@gmail.com>
2024-11-29 10:01:04 +0530

    Time: 5 ms (66.67%), Space: 9.7 MB (10%) - LeetHub

---
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.c
@@ -0,0 +1,42 @@
+static int dirs[][4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+void dfs(int r, int c, int** grid, bool** vis, int n, int m) {
+    vis[r][c] = true;
+    for (int i = 0; i < 4; ++i) {
+        int nr = r + dirs[i][0];
+        int nc = c + dirs[i][1];
+        if (nr >= 0 && nr < n && nc >= 0 && nc < m && !grid[nr][nc] &&
+            !vis[nr][nc])
+            dfs(nr, nc, grid, vis, n, m);
+    }
+}
+
+int closedIsland(int** grid, int gridSize, int* gridColSize) {
+    int n = gridSize, m = (*gridColSize);
+    bool** vis = (bool**)malloc(sizeof(bool*) * n);
+    for (int i = 0; i < n; ++i) {
+        vis[i] = (bool*)malloc(sizeof(bool) * m);
+        for (int j = 0; j < m; ++j)
+            vis[i][j] = false;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+                if (!grid[i][j] && !vis[i][j])
+                    dfs(i, j, grid, vis, n, m);
+            }
+        }
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (!grid[i][j] && !vis[i][j]) {
+                cnt++;
+                dfs(i, j, grid, vis, n, m);
+            }
+        }
+    }
+    return cnt;
+}
