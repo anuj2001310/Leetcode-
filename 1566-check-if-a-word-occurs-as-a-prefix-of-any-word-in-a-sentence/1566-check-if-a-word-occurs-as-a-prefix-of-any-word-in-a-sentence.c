db28bb4c8aaa0d12374d50a2473047f490c9222d
anuj2001310 <thakuranujbhati310@gmail.com>
2024-12-02 08:59:14 +0530

    Time: 2 ms (100%), Space: 7.9 MB (31.58%) - LeetHub

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.c
@@ -0,0 +1,42 @@
+typedef struct string {
+    char* str;
+    int size;
+} string;
+
+string* init(int cap) {
+    string* st = (string*)malloc(sizeof(string));
+    st->str = (char*)malloc(sizeof(char) * cap);
+    st->size = 0;
+    return st;
+}
+bool isPrefix(char* s, char* target) {
+    int n = strlen(target), m = strlen(target);
+    if (n > m)
+        return false;
+    int j = 0;
+    for (int i = 0; target[i] && s[j]; ++i) {
+        if (target[i] != s[j])
+            return false;
+        else
+            j++;
+    }
+    return true;
+}
+int isPrefixOfWord(char* sentence, char* searchWord) {
+    char** words = (char**)malloc(sizeof(char*) * 100);
+    int idx = 0;
+    string* h = init(100);
+    for (int i = 0; sentence[i]; ++i) {
+        if (sentence[i] == ' ') {
+            words[idx++] = h->str;
+            h = init(100);
+        } else
+            h->str[h->size++] = sentence[i];
+    }
+    words[idx++] = h->str;
+    for (int i = 0; i < idx; i++) {
+        if (isPrefix(words[i], searchWord))
+            return i + 1;
+    }
+    return -1;
+}
