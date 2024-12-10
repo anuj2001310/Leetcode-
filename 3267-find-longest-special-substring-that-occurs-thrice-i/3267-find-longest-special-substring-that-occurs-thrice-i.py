975869c759be607b737c78217bab8237ef88fe14
anuj2001310 <thakuranujbhati310@gmail.com>
2024-12-10 18:18:40 +0530

    Time: 5 ms (92.86%), Space: 12.3 MB (5.88%) - LeetHub

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def maximumLength(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = -1
+        dic = {}
+        l = 0
+        for i, c in enumerate(s):
+            if c not in dic:
+                dic[c] = []
+                heapq.heapify(dic[c])
+            if c != s[l]:
+                ml = i - l
+                heapq.heappush(dic[s[l]], ml)
+                if len(dic[s[l]]) > 3:
+                    heapq.heappop(dic[s[l]])
+                l = i
+        ml = len(s) - l
+        heapq.heappush(dic[s[l]], ml)
+        if len(dic[s[-1]]) > 3:
+            heapq.heappop(dic[s[-1]])
+        for c in dic:
+            arr = dic[c]
+            arr.sort(reverse=True)
+            res = max(res, arr[0] - 2)
+            if len(arr) > 2:
+                if arr[0] == arr[1] and arr[1] == arr[2]:
+                    res = max(res, arr[0])
+                if arr[0] > arr[1]:
+                    res = max(res, arr[1])
+                else:
+                    res = max(res, arr[1] - 1)
+            elif len(arr) == 2:
+                if arr[0] > arr[1]:
+                    res = max(res, arr[1])
+                else:
+                    
+                    res = max(res, arr[1] - 1)
+        return res if res > 0 else -1
