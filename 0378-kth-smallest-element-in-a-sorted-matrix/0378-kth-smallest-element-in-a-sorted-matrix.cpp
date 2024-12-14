6c693bfaa70d37b6f905cb8a24e14bde79871372
anuj2001310 <thakuranujbhati310@gmail.com>
2024-12-14 10:57:41 +0530

    Time: 14 ms (22.91%), Space: 17.6 MB (53.07%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,16 +1,40 @@
+typedef pair<int, int> pii;
+typedef pair<int, pii> pipii;
+
 class Solution {
+/*
+private:
+    struct Compare {
+        bool operator()(pipii x, pipii y) {
+            if (x.first > y.first)
+                return true;
+            return false;
+        }
+    };
+*/
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        int n = matrix.size(), m = matrix[0].size();
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j)
-                pq.push(matrix[i][j]);
-        }
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        priority_queue<pipii, vector<pipii>, greater<pipii>> pq;
 
+        vis[0][0] = true;
+        pq.push({matrix[0][0], {0, 0}});
         while (--k && pq.size() > 0) {
+            int val = pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
             pq.pop();
+            if (r + 1 < n and !vis[r + 1][c]) {
+                vis[r + 1][c] = true;
+                pq.push({matrix[r + 1][c], {r + 1, c}});
+            }
+            if (c + 1 < m and !vis[r][c + 1]) {
+                vis[r][c + 1] = true;
+                pq.push({matrix[r][c + 1], {r, c + 1}});
+            }
         }
-        return pq.top();
+        return pq.top().first;
     }
 };
