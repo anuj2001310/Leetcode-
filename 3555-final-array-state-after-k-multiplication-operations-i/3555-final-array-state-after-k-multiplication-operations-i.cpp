872dad4a09546196492360266b1c2fdb95bf29bc
anuj2001310 <thakuranujbhati310@gmail.com>
2024-12-16 20:22:11 +0530

    Time: 4 ms (20.56%), Space: 27.6 MB (29.75%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        int n = nums.size();
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+
+        for (int i = 0; i < n; i++)
+            minHeap.push({nums[i], i});
+
+        while (k--) {
+            auto [value, index] = minHeap.top();
+            nums[index] = nums[index] * multiplier;
+            minHeap.pop();
+            minHeap.push({nums[index], index});
+        }
+        return nums;
+    }
+};
