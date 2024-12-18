e06d8b14f85188065de598d4ed16f8bc6a99148a
anuj2001310 <thakuranujbhati310@gmail.com>
2024-12-18 08:10:54 +0530

    Time: 106 ms (5%), Space: 46.6 MB (66.25%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] FinalPrices(int[] prices) {
+        int n = prices.Length;
+        int[] help = new int[n];
+        int x = 0;
+        for (int i = 0; i < n - 1; i++) {
+            int dis = prices[i];
+            for (int j = i + 1; j < n; j++) {
+                if (prices[j] <= prices[i]) {
+                    dis = prices[i] - prices[j];
+                    break;
+                }
+            }
+            help[x++] = dis;
+        }
+        help[x] = prices[n - 1];
+        return help;
+    }
+}
