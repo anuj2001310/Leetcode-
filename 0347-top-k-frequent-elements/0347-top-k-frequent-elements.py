b0147ac03176af2728af444b75fe5e70f455aedd
anuj2001310 <thakuranujbhati310@gmail.com>
2024-12-18 18:03:24 +0530

    Time: 3 ms (91.86%), Space: 21.3 MB (25.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,21 +1,15 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        maps = {}
-        for ele in nums:
-            maps[ele] = maps.get(ele, 0) + 1
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqs = {}
+        for num in nums:
+            freqs[num] = freqs.get(num, 0) - 1
         
-        queue = []
-        nums_d = set(nums)
-        for num in nums_d:
-            heapq.heappush(queue, (-maps[num], num))
-        
-        ans = []
-        while len(ans) < k:
-            ans.append(queue[0][1])
-            heapq.heappop(queue)
-        return ans
+        freqs = [(val, key) for key, val in freqs.items()]
+        heapq.heapify(freqs)
+
+        i = 0
+        out = []
+        while i < k:
+            out.append(heapq.heappop(freqs)[1])
+            i += 1
+        return out 
