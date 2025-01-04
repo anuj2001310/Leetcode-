5fd8b28eaa1b37e289f1faa973ac555c6f3b87d0
anuj2001310 <thakuranujbhati310@gmail.com>
2025-01-04 09:39:27 +0530

    Time: 0 ms (100%), Space: 7.9 MB (43%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.go
@@ -0,0 +1,24 @@
+func setZeroes(matrix [][]int)  {
+    n := len(matrix)
+    m := len(matrix[0])
+
+    row := make([]bool, n)
+    col := make([]bool, m)
+
+    for i := 0; i < n; i++ {
+        for j := 0; j < m; j++ {
+            if matrix[i][j] == 0 {
+                row[i] = true
+                col[j] = true
+            }
+        }
+    }
+
+    for i := 0; i < n; i++ {
+        for j := 0; j < m; j++ {
+            if row[i] || col[j] {
+                matrix[i][j] = 0
+            }
+        }
+    }
+}
