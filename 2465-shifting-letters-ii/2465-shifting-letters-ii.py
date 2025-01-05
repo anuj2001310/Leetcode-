4181ff58b053a394039e5f07736813a4e2b4afa7
anuj2001310 <thakuranujbhati310@gmail.com>
2025-01-05 17:49:45 +0530

    Time: 99 ms (42.31%), Space: 30.6 MB (15.38%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def shiftingLetters(self, s, shifts):
+        """
+        :type s: str
+        :type shifts: List[List[int]]
+        :rtype: str
+        """
+        n = len(s)
+        prefix = [0] * (n + 1)
+
+        for shift in shifts:
+            st, ed, dr = shift
+            prefix[st] += (2 * dr - 1)
+            if ed + 1 < n:
+                prefix[ed + 1] -= (2 * dr - 1)
+
+        sb = []
+        currShift = 0
+
+        for i in range(n):
+            currShift = (currShift + prefix[i]) % 26
+            ch = chr((ord(s[i]) - ord('a') + currShift + 26) % 26 + ord('a'))
+            sb.append(ch)
+
+        return ''.join(sb)
