3f9b9af8e286cb08c4e4d4f5802b6cd9347b0d70
anuj2001310 <thakuranujbhati310@gmail.com>
2025-01-06 10:27:42 +0530

    Time: 9 ms (80.72%), Space: 12.5 MB (59.04%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,29 +1,22 @@
-class Solution:
-    def minOperations(self, boxes: str) -> List[int]:
+class Solution(object):
+    def minOperations(self, boxes):
+        """
+        :type boxes: str
+        :rtype: List[int]
+        """
         n = len(boxes)
-        prefix_right = [0 for _ in range(n)]
-        prefix_left = [0 for _ in range(n)]
+        res = [0] * n
 
-        #Scan from left --> right
+        balls, moves = 0, 0
         for i in range(n):
-            temp = 0
-            for j in range(i + 1, n):
-                if boxes[j] == '1':
-                    temp += abs(i - j)
-            
-            prefix_right[i] = temp
+            res[i] = (balls + moves)
+            moves += balls
+            balls += (ord(boxes[i]) - ord('0'))
         
-        #Scan from left <-- right
-        for i in range(n - 1, -1, -1):
-            temp = 0
-            for j in range(i - 1, -1, -1):
-                if boxes[j] == '1':
-                    temp += abs(i - j)
-
-            prefix_left[i] = temp
-
-        #print(prefix_right)
-        #print(prefix_left)
+        balls, moves = 0, 0
+        for i in reversed(range(n)):
+            res[i] += (balls + moves)
+            moves += balls
+            balls += (ord(boxes[i]) - ord('0'))
         
-        #Add both the arrays 
-        return [prefix_left[i] + prefix_right[i] for i in range(n)]
+        return res
