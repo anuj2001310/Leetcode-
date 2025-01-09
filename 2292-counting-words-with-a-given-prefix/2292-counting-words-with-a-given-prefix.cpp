ad747af599f690b963cf0142c309babac27c1d7b
anuj2001310 <thakuranujbhati310@gmail.com>
2025-01-09 10:42:37 +0530

    Time: 0 ms (100%), Space: 12.9 MB (99.15%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(NULL);
-        cout.tie(NULL);
         int cnt = 0;
         for (auto& str : words) {
-            if (str.substr(0, pref.length()) == pref)
+            if (str.starts_with(pref))
                 cnt++;
         }
         return cnt;
