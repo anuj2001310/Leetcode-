d68e2a57ff2f32fb7eb629bdd822e132b44b8fcc
anuj2001310 <thakuranujbhati310@gmail.com>
2025-01-10 23:41:35 +0530

    Time: 11 ms (91.32%), Space: 61.7 MB (91.32%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool help(int f[], int mf[]) {
+        for (int i = 0; i < 26; i++) {
+            if (f[i] < mf[i])
+                return false;
+        }
+        return true;
+    }
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> ans;
+        int mf[26] = {0};
+        for (string word : words2) {
+            int f[26] = {0};
+            for (char s : word) 
+                f[s - 'a'] += 1;
+        
+            for (int i = 0; i < 26; i++)
+                mf[i] = max(mf[i], f[i]);
+        }
+
+        for (string word : words1) {
+            int f[26] = {0};
+            for (char s : word)
+                f[s - 'a'] += 1;
+            
+            if (help(f, mf)) 
+                ans.push_back(word);
+                
+        }
+        return ans;
+    }
+};
