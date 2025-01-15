3131206d2f1cc5bcef97b9b5e916e6fb5bac111d
anuj2001310 <thakuranujbhati310@gmail.com>
2025-01-15 16:54:17 +0530

    Time: 0 ms (100%), Space: 8.1 MB (10.75%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimizeXor(int x, int y) {
+        int b = __builtin_popcount(y);
+        int A = 0;
+
+        for (int i = 31; i >= 0 && b; i--)
+            if (x & (1 << i)) {
+                b--, A |= (1 << i);
+            }
+
+        for (int i = 0; i <= 31 && b; i++)
+            if ((A & (1 << i)) == 0) {
+                b--, A |= (1 << i);
+            }
+
+        return A;
+    }
+};
