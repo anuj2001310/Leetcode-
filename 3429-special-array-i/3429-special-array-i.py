8d9dd0348efc69bbbacc2d88c5a92a16ec7bb5fe
anuj2001310 <thakuranujbhati310@gmail.com>
2025-02-01 14:46:34 +0530

    Time: 2 ms (27.43%), Space: 17.7 MB (33.33%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.py
+++ 3429-special-array-i/3429-special-array-i.py
@@ -1,7 +1,3 @@
-class Solution(object):
-    def isArraySpecial(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
         return all((nums[i] & 1) != (nums[i - 1] & 1) for i in range(1, len(nums)))
