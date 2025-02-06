6fe5943d1b733da228d82f160b1cebcaa16cd3c6
anuj2001310 <thakuranujbhati310@gmail.com>
2025-02-06 17:25:08 +0530

    Time: 291 ms (95.73%), Space: 46.3 MB (65.4%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -1,14 +1,8 @@
-class Solution(object):
-    def tupleSameProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        n = len(nums)
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
         map = {}
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                p = nums[i] * nums[j]
-                map[p] = map.get(p, 0) + 1
-        
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                map[nums[i] * nums[j]] = map.get(nums[i] * nums[j], 0) + 1  
+
         return sum(((w * (w - 1)) * 4) for w in map.values() if w > 1)
