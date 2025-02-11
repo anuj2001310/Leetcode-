d41e530ed123faca0d3964ee32005f87d1e32d62
anuj2001310 <thakuranujbhati310@gmail.com>
2025-02-11 15:59:28 +0530

    Time: 0 ms (100%), Space: 17.8 MB (75.36%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def removeOccurrences(self, s, part):
-        """
-        :type s: str
-        :type part: str
-        :rtype: str
-        """
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
         while part in s:
             s = s.replace(part, "", 1)  # Remove first occurrence of part
         return s
