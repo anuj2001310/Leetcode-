f29bd5279a1a5c732669f602055ba91fd1950719
anuj2001310 <thakuranujbhati310@gmail.com>
2025-03-13 16:11:46 +0530

    Time: 83 ms (41.18%), Space: 87.4 MB (13.84%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -0,0 +1,27 @@
+typedef vector<int> vi;
+typedef pair<int, vi> pivi;
+class Solution {
+private:
+    int distance(int x, int y) { return pow(x, 2) + pow(y, 2); }
+
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        int n = points.size();
+        priority_queue<pivi, vector<pivi>, greater<pivi>> pq;
+        for (int i = 0; i < n; ++i) {
+            int x = points[i][0];
+            int y = points[i][1];
+            int dis = distance(x, y);
+            pq.push({dis, points[i]});
+        }
+
+        vector<vector<int>> ans;
+        while (k-- > 0 && !pq.empty()) {
+            auto curr = pq.top();
+            pq.pop();
+            ans.push_back(curr.second);
+        }
+
+        return ans;
+    }
+};
