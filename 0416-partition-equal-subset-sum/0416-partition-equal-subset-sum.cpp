8a060aa6e308bf4cb38ff087047dca494864b35e
anuj2001310 <thakuranujbhati310@gmail.com>
2025-03-21 11:57:55 +0530

    Time: 177 ms (95.12%), Space: 100.4 MB (38.42%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    bool memo(vector<int>& nums, vector<vector<int>>& dp, int sum, int idx) {
+        if (sum == 0)
+            return true;
+
+        if (idx >= nums.size())
+            return false;
+
+        if (dp[idx][sum] != -1)
+            return dp[idx][sum];
+
+        bool pick = false, non_pick = false;
+
+        if (nums[idx] <= sum)
+           pick = memo(nums, dp, sum - nums[idx], idx + 1);
+
+        non_pick = memo(nums, dp, sum, idx + 1);
+
+        return dp[idx][sum] = (pick or non_pick);
+    }
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        if (sum & 1)
+            return false;
+
+        vector<vector<int>> dp (n, vector<int> ((sum >> 1) + 1,  -1));
+        
+        return memo(nums, dp, sum / 2, 0);
+    }
+};
