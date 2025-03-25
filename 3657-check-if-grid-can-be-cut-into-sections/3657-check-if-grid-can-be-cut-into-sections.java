171b29344f8f7ae82249f523d83f4dc1baa635da
anuj2001310 <thakuranujbhati310@gmail.com>
2025-03-25 10:45:56 +0530

    Time: 82 ms (99.56%), Space: 117.6 MB (89.91%) - LeetHub

--- 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.java
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.java
@@ -1,26 +1,22 @@
 class Solution {
-
-    public boolean checkValidCuts(int n, int[][] rectangles) {
-        return checkCuts(rectangles, 0) || checkCuts(rectangles, 1);
-    }
-
-    private boolean checkCuts(int[][] rectangles, int dim) {
-        int gapCount = 0;
-
-        Arrays.sort(rectangles, (a, b) -> Integer.compare(a[dim], b[dim]));
-
+    private boolean help(int[][] rectangles, int dim) {
+        int gap = 0;
+        Arrays.sort(rectangles, (a, b) -> a[dim] - b[dim]);
         int furthestEnd = rectangles[0][dim + 2];
 
         for (int i = 1; i < rectangles.length; i++) {
             int[] rect = rectangles[i];
 
-            if (furthestEnd <= rect[dim]) {
-                gapCount++;
-            }
+            if (furthestEnd <= rect[dim])
+                gap++;
 
             furthestEnd = Math.max(furthestEnd, rect[dim + 2]);
         }
 
-        return gapCount >= 2;
+        return gap >= 2;
+    }
+
+    public boolean checkValidCuts(int n, int[][] rectangles) {
+        return help(rectangles, 0) || help(rectangles, 1);
     }
 }
