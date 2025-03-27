0c1573a28a081f2f5971492828810b42dcb54639
anuj2001310 <thakuranujbhati310@gmail.com>
2025-03-27 18:12:04 +0530

    Time: 0 ms (100%), Space: 17.9 MB (32.4%) - LeetHub

--- 3625-stone-removal-game/3625-stone-removal-game.py
+++ 3625-stone-removal-game/3625-stone-removal-game.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def canAliceWin(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
+class Solution:
+    def canAliceWin(self, n: int) -> bool:
         for i in range(10, -1, -1):
             if n < i:
                 return i & 1 != 0
