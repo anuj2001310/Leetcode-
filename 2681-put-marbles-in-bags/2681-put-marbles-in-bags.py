2d276364350054ab2154661c9cceb64e44562fef
anuj2001310 <thakuranujbhati310@gmail.com>
2025-03-31 14:45:01 +0530

    Time: 158 ms (19.84%), Space: 30.6 MB (21.07%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.py
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.py
@@ -1,12 +1,6 @@
-class Solution(object):
-    def putMarbles(self, weights, k):
-        """
-        :type weights: List[int]
-        :type k: int
-        :rtype: int
-        """
-
-        p = [0 for _ in range(len(weights) - 1)]
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        p = [0] * (len(weights) - 1)
         for i in range(len(weights) - 1):
             p[i] += weights[i] + weights[i + 1]
         p.sort()
