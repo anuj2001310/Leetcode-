aea26b6e2edd33ea450c7a9c4eaf74f84541c368
anuj2001310 <thakuranujbhati310@gmail.com>
2025-04-04 15:37:23 +0530

    Time: 2 ms (62.96%), Space: 18.3 MB (25.86%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
@@ -1,28 +1,22 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def lcaDeepestLeaves(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: Optional[TreeNode]
-        """
-        def dfs (root):
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(root):
             if not root:
-                return [None, 0]
-        
-            left = dfs (root.left)
-            right = dfs (root.right)
+                return 0, None
 
-            if left[1] > right[1]:
-                return [left[0], left[1] + 1]
-        
-            if left[1] < right[1]:
-                return [right[0], right[1] + 1]
-                
-            return [root, left[1] + 1]
-    
-        return dfs(root)[0]
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            if left[0] > right[0]:
+                return left[0] + 1, left[1]
+            if left[0] < right[0]:
+                return right[0] + 1, right[1]
+            return left[0] + 1, root
+
+        return dfs(root)[1]
