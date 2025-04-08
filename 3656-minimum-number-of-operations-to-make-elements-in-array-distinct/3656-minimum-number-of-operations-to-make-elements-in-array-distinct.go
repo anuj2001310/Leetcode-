c8e6bf2e5039b0cd286d5f40d0cd92b1fd0cfde2
anuj2001310 <thakuranujbhati310@gmail.com>
2025-04-08 10:51:53 +0530

    Time: 0 ms (100%), Space: 4.7 MB (86.05%) - LeetHub

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.go
@@ -0,0 +1,12 @@
+func minimumOperations(nums []int) int {
+    n := len(nums)
+    freq := make([]bool, 101)
+
+    for i := n - 1; i >= 0; i-- {
+        if freq[nums[i]] {
+            return (i / 3) + 1
+        }
+        freq[nums[i]] = true
+    }
+    return 0
+}
