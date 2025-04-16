5cd68431ee57baceb4cfd4ab9d00863b3d681e54
anuj2001310 <thakuranujbhati310@gmail.com>
2025-04-16 17:08:46 +0530

    Time: 0 ms (100%), Space: 55.7 MB (24.69%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function(x, y, z) {
+    let d1 = Math.abs(x - z);
+    let d2 = Math.abs(y - z);
+    if (d1 < d2)
+        return 1;
+    else if (d1 == d2)
+        return 0;
+    return 2;
+};
