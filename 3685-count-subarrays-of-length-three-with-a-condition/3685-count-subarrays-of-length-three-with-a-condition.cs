d775ed963c3119db124ae97ff1e5d41e1d244ef0
anuj2001310 <thakuranujbhati310@gmail.com>
2025-04-27 21:49:28 +0530

    Time: 1 ms (100%), Space: 48.7 MB (82.22%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cs
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cs
@@ -1,10 +1,11 @@
 public class Solution {
     public int CountSubarrays(int[] nums) {
-        int cnt = 0;
-        int n = nums.Length;
-        for (int i = 1; i < n - 1; ++i)
-            cnt += (2 * (nums[i - 1] + nums[i + 1]) == nums[i]) ? 1 : 0;
-        
+        var n = nums.Length;
+        var cnt = 0;
+        for (int i = 1; i < n  - 1; ++i) {
+            if (2 * (nums[i - 1] + nums[i + 1]) == nums[i])
+                cnt++;
+        }
         return cnt;
     }
 }
