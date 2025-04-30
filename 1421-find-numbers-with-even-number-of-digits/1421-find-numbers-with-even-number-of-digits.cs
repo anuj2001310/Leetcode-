97191430a55167a8250324a7164eb0e47d7507b3
anuj2001310 <thakuranujbhati310@gmail.com>
2025-04-30 12:50:11 +0530

    Time: 1 ms (57.93%), Space: 42.6 MB (44.03%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cs
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cs
@@ -1,14 +1,14 @@
 public class Solution {
     public int FindNumbers(int[] nums) {
         var cnt = 0;
-        foreach (int num in nums) {
-            int s = 0;
-            int n = num;
-            while (n != 0) {
-                s++;
-                n /= 10;
-            } 
-            if ((s & 1) == 0)
+        foreach (var n in nums) {
+            int num = n;
+            int size = 0;
+            while (num != 0) {
+                num /= 10;
+                size++;
+            }
+            if ((size & 1) == 0)
                 cnt++;
         }
         return cnt;
