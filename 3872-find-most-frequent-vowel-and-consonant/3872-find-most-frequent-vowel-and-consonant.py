277894e0e50d0b894c31575ea9fcb204fc240ca4
anuj2001310 <thakuranujbhati310@gmail.com>
2025-05-11 13:36:12 +0530

    Time: 4 ms (0%), Space: 17.8 MB (100%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        n = len(s)
+        freq = [0 for _ in range(26)]
+        v, c = 0, 0
+        for ch in s:
+            idx = ord(ch) - ord('a')
+            freq[idx] += 1
+
+        def isVowel(ch : chr) -> bool:
+            return ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u'
+
+        
+        for i in range(n):
+            idx = ord(s[i]) - ord('a')
+            if isVowel(s[i]):
+                v = max(v, freq[idx])
+            else:
+                c = max(c, freq[idx])
+        return v + c
