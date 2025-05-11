bc433cd21ca78e2149e1b7b4511aad7ee75840c9
anuj2001310 <thakuranujbhati310@gmail.com>
2025-05-11 13:34:45 +0530

    Time: 3 ms (100%), Space: 57.6 MB (100%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxFreqSum = function(s) {
+    const freq = Array(26).fill(0);
+    let n = s.length;
+
+    let c = 0, v = 0;
+    for (let i = 0; i < n; ++i) {
+        let idx = s.charCodeAt(i) - 97;
+        freq[idx]++;
+    }
+    
+    const isVowel = (ch) => {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+    
+    for (let i = 0; i < n; ++i) {
+        let idx = s.charCodeAt(i) - 97;
+        if (isVowel(s[i]))
+            v = (freq[idx] > v ? freq[idx] : v);
+        else
+            c = (freq[idx] > c ? freq[idx] : c);
+    }
+        
+    return c + v;
+};
