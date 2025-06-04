dae87e6718894ac43a806a660106db612bcf2b91
anuj2001310 <thakuranujbhati310@gmail.com>
2025-06-04 10:39:40 +0530

    Time: 7 ms (47.25%), Space: 19.2 MB (60.78%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         V = numCourses
         adj = [[] for _ in range(V)]
         indegree = [0 for _ in range(V)]
@@ -16,13 +11,14 @@ class Solution(object):
         
         cnt = 0
         q = deque([x for x in range(V) if indegree[x] == 0])
+
         while q:
             node = q.popleft()
             cnt += 1
+            
             for neigh in adj[node]:
                 indegree[neigh] -= 1
                 if indegree[neigh] == 0:
                     q.append(neigh)
 
         return cnt == V
-        
