5aec80f27b58183935b3e87b04058f18d885d66c
anuj2001310 <thakuranujbhati310@gmail.com>
2025-06-24 19:40:48 +0530

    Time: 0 ms (100%), Space: 45.9 MB (96%) - LeetHub

---
+++ 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool CheckPrimeFrequency(int[] nums) {
+        bool isPrime(int n) {
+            if (n <= 1)
+                return false;
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        
+        int n = nums.Length;
+        var map = new int[101];
+        for (int i = 0; i < n; i++)
+            map[nums[i]]++;
+
+        for (int i = 0; i < 101; ++i) {
+            if (map[i] == 0)
+                continue;
+            if (isPrime(map[i]))
+                return true;
+        }
+        return false;
+    }
+}
