81eb568df5c279fa61ac6948693346fe875220a9
anuj2001310 <thakuranujbhati310@gmail.com>
2025-07-12 16:11:57 +0530

    Time: 0 ms (100%), Space: 14.2 MB (97.12%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    void dfs (auto node, unordered_map<int, list<int>>& adj, vector<bool>& vis) {
+    void dfs (auto node, vector<vector<int>>& adj, vector<bool>& vis) {
         vis[node] = true;
         for (auto& neigh : adj[node]) {
             if (!vis[neigh])
@@ -11,14 +11,7 @@ public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
         vector<bool> vis(n, false);
-        unordered_map<int, list<int>> adj;
-        for (int i = 0; i < n; i++) {
-            for (auto& room : rooms[i]) {
-                int u = i;
-                int v = room;
-                adj[u].push_back(v);
-            }
-        }
+        
         int cnt = 0;
         /*
         for (auto& [u, v] : adj) {
@@ -30,7 +23,7 @@ public:
         for (int i = 0; i < n; i++) {
             if (!vis[i]) {
                 cnt++;
-                dfs(i, adj, vis);
+                dfs(i, rooms, vis);
             }
         }
         return cnt == 1;
