976e63da7a09e6a22951ff3a5ba001d7de83854a
anuj2001310 <thakuranujbhati310@gmail.com>
2025-07-21 13:45:26 +0530

    Time: 552 ms (48.29%), Space: 122.1 MB (42.86%) - LeetHub

--- 3823-count-islands-with-total-value-divisible-by-k/3823-count-islands-with-total-value-divisible-by-k.py
+++ 3823-count-islands-with-total-value-divisible-by-k/3823-count-islands-with-total-value-divisible-by-k.py
@@ -1,10 +1,15 @@
-class Solution:
-    def countIslands(self, grid: List[List[int]], k: int) -> int:
+class Solution(object):
+    def countIslands(self, grid, k):
+        """
+        :type grid: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
         n, m = len(grid), len(grid[0])
         vis = [[False for _ in range(m)] for _ in range(n)]
         ct = 0
 
-        def dfs (r: int, c: int) -> int:
+        def dfs (r, c):
             if r >= n or r < 0 or c >= m or c < 0 or vis[r][c] or grid[r][c] == 0:
                 return 0
             vis[r][c] = True
@@ -14,7 +19,6 @@ class Solution:
             s += dfs (r, c + 1)
             s += dfs (r + 1, c)
             s += dfs (r, c - 1)
-            
             return s
             
         for i in range(n):
