f5bbd5eee32371bb8130bb75c9b3248dc74ba70c
anuj2001310 <thakuranujbhati310@gmail.com>
2025-08-27 18:31:22 +0530

    Time: 132 ms (89.84%), Space: 102.3 MB (83.42%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,43 +1,59 @@
+constexpr int d[5] = {1, 1, -1, -1, 1}; //(1,1),(1,-1),(-1, -1),(-1, 1)
+array<array<int, 2>, 4> dp[500][500];
 class Solution {
 public:
-    int lenOfVDiagonal(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        int dirs[4][2] = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
-        int memo[m][n][4][2];
-        memset(memo, -1, sizeof(memo));
+    int n, m;
 
-        function<int(int, int, int, bool, int)> dfs =
-            [&](int cx, int cy, int direction, bool turn, int target) -> int {
-            int nx = cx + dirs[direction][0];
-            int ny = cy + dirs[direction][1];
-            /* If it goes beyond the boundary or the next node's value is not
-             * the target value, then return */
-            if (nx < 0 || ny < 0 || nx >= m || ny >= n ||
-                grid[nx][ny] != target) {
-                return 0;
+    bool isOutSide(int i, int j) { return i < 0 || i >= n || j < 0 || j >= m; }
+    const int nxt[3] = {2, 2, 0};
+
+    int dfs(int i, int j, int dir, bool turn, vector<vector<int>>& grid) {
+        if (dp[i][j][dir][turn] != -1)
+            return dp[i][j][dir][turn];
+
+        int ans = 1, x = grid[i][j];
+
+        // Move in the current direction
+        int s = i + d[dir], t = j + d[dir + 1];
+        if (!isOutSide(s, t)) {
+            int y = grid[s][t];
+            if (nxt[x] ==
+                y) // Condition to continue moving in the same diagonal
+                ans = max(ans, 1 + dfs(s, t, dir, turn, grid));
+        }
+
+        // Try turning to the next diagonal direction
+        if (!turn) {
+            int newDir = (dir + 1) % 4;
+            int u = i + d[newDir], v = j + d[newDir + 1];
+            if (!isOutSide(u, v)) {
+                int z = grid[u][v];
+                if (nxt[x] == z) // Condition to turn to a different diagonal
+                    ans = max(ans, 1 + dfs(u, v, newDir, 1, grid));
             }
-            if (memo[nx][ny][direction][turn] != -1)
-                return memo[nx][ny][direction][turn];
-            
-            int maxStep = dfs(nx, ny, direction, turn, 2 - target);
-            if (turn) 
-                maxStep = fmax(maxStep, dfs(nx, ny, (direction + 1) % 4, false,
-                                            2 - target));
-            memo[nx][ny][direction][turn] = maxStep + 1;
-            return maxStep + 1;
-        };
+        }
 
-        int res = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 1) {
-                    for (int direction = 0; direction < 4; ++direction) {
-                        res = fmax(res, dfs(i, j, direction, true, 2) + 1);
+        return dp[i][j][dir][turn] = ans;
+    }
+
+    int lenOfVDiagonal(vector<vector<int>>& grid) {
+        n = grid.size(), m = grid[0].size();
+        constexpr array<array<int, 2>, 4> m1{
+            {{-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}}};
+        for (int i = 0; i < n; i++)
+            fill(dp[i], dp[i] + m, m1);
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] ==
+                    1) { // Start from cells that are part of the path
+                    for (int d = 0; d < 4; d++) {
+                        ans = max(ans, dfs(i, j, d, 0, grid));
                     }
                 }
             }
         }
-
-        return res;
+        return ans;
     }
 };
