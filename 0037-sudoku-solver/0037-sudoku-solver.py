0dfca8d42c12362b4b10bd68f7c20f17b514840a
anuj2001310 <thakuranujbhati310@gmail.com>
2025-08-31 15:25:54 +0530

    Time: 1876 ms (50.78%), Space: 18.1 MB (39.52%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -3,62 +3,49 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxes = [set() for _ in range(9)]
+        rows = [0] * 9
+        cols = [0] * 9
+        boxes = [0] * 9
+
         for r in range(9):
             for c in range(9):
                 if board[r][c] != '.':
-                    rows[r].add(board[r][c])
-                    cols[c].add(board[r][c])
-                    box_id = r//3  * 3 + c//3 
-                    boxes[box_id].add(board[r][c])
-        
-        def select_candidates( r, c):
-            box_id = (r // 3) * 3 + (c // 3)
-            candidates = set()
-            for num in map(str, range(1, 10)):  # '1' to '9'
-                if num not in rows[r] and num not in cols[c] and num not in boxes[box_id]:
-                    candidates.add(num)
-            return candidates
+                    num = int(board[r][c])
+                    bit = 1 << (num - 1)
+                    box_idx = (r // 3) * 3 + (c // 3)
+                    rows[r] |= bit
+                    cols[c] |= bit
+                    boxes[box_idx] |= bit
 
-        def find_best_cell():
-            min_candidates = 9
-            best_cell = None
-            best_candidates = set()
-            for r in range(9):
-                for c in range(9):
-                    if board[r][c] == '.':
-                        cell_candidates = select_candidates(r, c)
-                        if len(cell_candidates) < min_candidates:
-                            min_candidates = len(cell_candidates)
-                            best_cell = (r, c)
-                            best_candidates = cell_candidates
-            return best_cell, best_candidates
-
-        def backtrack():
-            best_cell, candidates = find_best_cell()
-            if not best_cell:
-                # every cell is filled, sudoku is correct
+        def backtrack(r, c):
+            if r == 9:
                 return True
-            r,c = best_cell
-            for can in candidates:
-                #place num
-                board[r][c] = can
-                # add to the checking sets
-                rows[r].add(can)
-                cols[c].add(can)
-                box_id = r//3  * 3 + c//3 
-                boxes[box_id].add(can)
             
-                if backtrack():
-                    return True
-                # undo 
-                board[r][c] = '.'  
-                rows[r].remove(can)
-                cols[c].remove(can)
-                boxes[box_id].remove(can) 
+            next_r = r + (c + 1) // 9
+            next_c = (c + 1) % 9
 
-            return False      
+            if board[r][c] != '.':
+                return backtrack(next_r, next_c)
 
-        backtrack()
+            box_idx = (r // 3) * 3 + (c // 3)
+            for i in range(1, 10):
+                bit = 1 << (i - 1)
+                
+                if not (rows[r] & bit or cols[c] & bit or boxes[box_idx] & bit):
+                    num_str = str(i)
+                    board[r][c] = num_str
+                    rows[r] |= bit
+                    cols[c] |= bit
+                    boxes[box_idx] |= bit
+
+                    if backtrack(next_r, next_c):
+                        return True
+
+                    board[r][c] = '.'
+                    rows[r] ^= bit
+                    cols[c] ^= bit
+                    boxes[box_idx] ^= bit
+            
+            return False
+
+        backtrack(0, 0)
