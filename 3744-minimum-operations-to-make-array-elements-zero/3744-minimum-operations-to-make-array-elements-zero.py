e7c8d7c4ab8ae0826bdfb29d5e706f4d2a61e9cc
anuj2001310 <thakuranujbhati310@gmail.com>
2025-09-06 15:50:20 +0530

    Time: 866 ms (88.29%), Space: 54.9 MB (35.52%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.py
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.py
@@ -1,16 +1,23 @@
 class Solution:
-    def get(self, num: int) -> int:
-        i = 1
-        base = 1
-        cnt = 0
-        while base <= num:
-            cnt += ((i + 1) // 2) * (min(base * 2 - 1, num) - base + 1)
-            i += 1
-            base *= 2
-        return cnt
-
     def minOperations(self, queries: List[List[int]]) -> int:
-        res = 0
-        for q in queries:
-            res += (self.get(q[1]) - self.get(q[0] - 1) + 1) // 2
-        return res
+        intervals = []
+        for d in range(1, 16):
+            low = 4 ** (d - 1)
+            high = 4 ** d - 1
+            intervals.append((low, high, d))
+        
+        result = 0
+        for l, r in queries:
+            total_depth = 0
+            for low, high, d in intervals:
+                if l > high:
+                    continue
+                if r < low:
+                    break
+                low_actual = max(l, low)
+                high_actual = min(r, high)
+                if low_actual <= high_actual:
+                    count = high_actual - low_actual + 1
+                    total_depth += count * d
+            result += (total_depth + 1) // 2
+        return result
