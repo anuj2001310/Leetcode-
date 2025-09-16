a0dfb7c347b395f7878cded65dc9b5213eeccde4
anuj2001310 <thakuranujbhati310@gmail.com>
2025-09-16 08:41:41 +0530

    Time: 130 ms (60.87%), Space: 31.6 MB (36.96%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.py
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.py
@@ -1,24 +1,17 @@
 class Solution:
-    #def gcd(self, a : int, b : int) -> int:
-        #return a if b == 0 else self.gcd(b, a % b)
-
     def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        if n == 1:
-            return nums
+        res = []
 
-
-        stack = []
-        
-        stack.append(nums[0])
         i = 1
+        res.append(nums[0])
         while i < n:
-            checkwith = nums[i]
-            while stack and gcd(checkwith, stack[-1]) > 1:
-                val = stack.pop()
-                checkwith = lcm(checkwith, val)
+            check = nums[i]
+            while res and gcd(check, res[-1]) > 1:
+                val = res.pop()
+                check = lcm(check, val)
             
             i += 1
-            stack.append(checkwith)
+            res.append(check)
         
-        return stack
+        return res
