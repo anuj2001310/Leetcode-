77fe927b5eb2ac9171e13558f8de0665d6796cfe
anuj2001310 <thakuranujbhati310@gmail.com>
2025-10-11 23:54:21 +0530

    Time: 375 ms (93%), Space: 40.1 MB (68.75%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.py
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.py
@@ -1,16 +1,11 @@
 class Solution:
-    def maximumTotalDamage(self, power):
-        count = Counter(power)
-        vec = [(-(10**9), 0)]
-        for k in sorted(count.keys()):
-            vec.append((k, count[k]))
-        n = len(vec)
-        f = [0] * n
-        mx = 0
-        j = 1
-        for i in range(1, n):
-            while j < i and vec[j][0] < vec[i][0] - 2:
-                mx = max(mx, f[j])
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        cnt = Counter(power)
+        a = sorted(cnt.keys())
+        f = [0] * (len(a) + 1)
+        j = 0
+        for i, x in enumerate(a):
+            while a[j] < x - 2:
                 j += 1
-            f[i] = mx + vec[i][0] * vec[i][1]
-        return max(f)
+            f[i + 1] = max(f[i], f[j] + x * cnt[x])
+        return f[-1]
