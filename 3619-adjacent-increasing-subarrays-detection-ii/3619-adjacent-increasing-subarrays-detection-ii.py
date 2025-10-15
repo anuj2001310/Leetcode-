b4fd4094aaa034e3ca80c7dde27bc3c79c07f4ca
anuj2001310 <thakuranujbhati310@gmail.com>
2025-10-15 21:38:48 +0530

    Time: 1367 ms (65.81%), Space: 45.3 MB (100%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.py
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.py
@@ -1,12 +1,24 @@
 class Solution:
     def maxIncreasingSubarrays(self, nums: List[int]) -> int:
-        n = len(nums)
-        cnt, precnt, ans = 1, 0, 0
-        for i in range(1, n):
-            if nums[i] > nums[i - 1]:
-                cnt += 1
-            else:
-                precnt, cnt = cnt, 1
-            ans = max(ans, min(precnt, cnt))
-            ans = max(ans, cnt // 2)
-        return ans
+        arr_lens = []
+        curr_len = 0
+        for idx, num in enumerate(nums):
+            if idx == 0:
+                curr_len = 1
+            elif num <= nums[idx - 1]:
+                arr_lens.append(curr_len)
+                curr_len = 1
+            elif num > nums[idx - 1]:
+                curr_len += 1
+        arr_lens.append(curr_len)
+        if len(arr_lens) == 1:
+            return arr_lens[0] // 2
+        candidate1 = max(arr_lens) // 2
+        candidate2 = -1
+        i, j = 0, 1
+        while j < len(arr_lens):
+            if min(arr_lens[i], arr_lens[j]) > candidate2:
+                candidate2 = min(arr_lens[i], arr_lens[j])
+            i += 1
+            j += 1
+        return max(candidate1, candidate2)
