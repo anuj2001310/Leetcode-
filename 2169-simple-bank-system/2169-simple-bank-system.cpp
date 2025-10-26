a84cd97d849730f5eb00053d6e67fb0a087359e1
anuj2001310 <thakuranujbhati310@gmail.com>
2025-10-27 00:22:57 +0530

    Time: 10 ms (69.71%), Space: 120.8 MB (86.56%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.cpp
+++ 2169-simple-bank-system/2169-simple-bank-system.cpp
@@ -1,36 +1,49 @@
 class Bank {
-private:
     vector<long long> balance;
+    int n = 0;
 
 public:
-    Bank(vector<long long>& balance) : balance(balance) {}
+    Bank(vector<long long>& balance) : balance(move(balance)) {
+        n = this->balance.size();
+    }
+
+    bool isValid(int account) { return account >= 1 && account <= n; }
 
     bool transfer(int account1, int account2, long long money) {
-        if (account1 > balance.size() || account2 > balance.size() ||
-            balance[account1 - 1] < money) {
+        if (!isValid(account1) || !isValid(account2))
             return false;
-        }
-        balance[account1 - 1] -= money;
-        balance[account2 - 1] += money;
-        return true;
+        if (withdraw(account1, money))
+            return deposit(account2, money);
+        return false;
     }
 
     bool deposit(int account, long long money) {
-        if (account > balance.size()) {
+        if (!isValid(account))
             return false;
-        }
-        balance[account - 1] += money;
+        int index = account - 1;
+        balance[index] += money;
         return true;
     }
 
     bool withdraw(int account, long long money) {
-        if (account > balance.size() || balance[account - 1] < money) {
+        if (!isValid(account))
             return false;
+        int index = account - 1;
+        if (balance[index] >= money) {
+            balance[index] -= money;
+            return true;
         }
-        balance[account - 1] -= money;
-        return true;
+        return false;
     }
 };
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
 
 /**
  * Your Bank object will be instantiated and called as such:
