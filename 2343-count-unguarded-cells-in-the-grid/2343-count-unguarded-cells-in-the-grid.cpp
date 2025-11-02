77f9ab00e948d86a987214ac1fa2e0fe30d125bf
anuj2001310 <thakuranujbhati310@gmail.com>
2025-11-02 10:28:14 +0530

    Time: 46 ms (91.28%), Space: 173.2 MB (48.17%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+    int countUnguarded(int m, int n, vector<vector<int>>& guards,
+                       vector<vector<int>>& walls) {
         vector<vector<int>> vis(m, vector<int>(n, 0));
         for (auto& arr : walls)
             vis[arr[0]][arr[1]] = 3;
@@ -47,7 +48,7 @@ public:
                 down++;
             }
         }
-        
+
         int cnt = 0;
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; ++j)
