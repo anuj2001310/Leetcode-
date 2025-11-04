a4eaa80b9e18845eca9dd76df435c9b3f810bdeb
anuj2001310 <thakuranujbhati310@gmail.com>
2025-11-05 01:51:41 +0530

    Time: 0 ms (100%), Space: 9.1 MB (35.47%) - LeetHub

---
+++ 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.c
@@ -0,0 +1,15 @@
+int missingMultiple(int* nums, int numsSize, int k) {
+    bool* vis = (bool*)calloc(sizeof(bool), 101);
+
+    for (int i = 0; i < numsSize; ++i)
+        vis[nums[i]] = true;
+    int ans = k;
+    for (; ans < 101; ans += k) {
+        if (ans % k == 0 && !vis[ans]) {
+            free(vis);
+            return ans;
+        }
+    }
+    free(vis);
+    return ans;
+}
