b4c639b7b7b3c86fde2629767712fa6963450e32
anuj2001310 <thakuranujbhati310@gmail.com>
2025-11-09 23:54:22 +0530

    Time: 0 ms (100%), Space: 5.6 MB (100%) - LeetHub

---
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.c
@@ -0,0 +1,11 @@
+int countOperations(int n1, int n2) {
+    int ct = 0;
+    while (n1 != 0 && n2 != 0) {
+        ++ct;
+        if (n1 > n2)
+            n1 -= n2;
+        else if (n2 >= n1)
+            n2 -= n1;
+    }
+    return ct;
+}
