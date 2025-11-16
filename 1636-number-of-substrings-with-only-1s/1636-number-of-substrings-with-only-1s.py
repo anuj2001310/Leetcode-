329d4893abbfd8b7b2f71cf6732404b6f8e1e15c
anuj2001310 <thakuranujbhati310@gmail.com>
2025-11-16 23:09:35 +0530

    Time: 45 ms (5.62%), Space: 18.2 MB (54.31%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.py
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.py
@@ -1,15 +1,22 @@
-class Solution(object):
-    def numSub(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        substring = s.split('0')
-        MOD = 10 ** 9 + 7
-        ANS = 0
+class Solution:
+    def numSub(self, str: str) -> int:
+        mod = 10 ** 9 + 7
+        n = len(str)
+        t, s = 0, 0
+        r = 0
+        ans = 0
 
-        for substr in substring:
-            count = len(substr)
-            ANS = (ANS + (count * (count + 1)) // 2) % MOD
+        while r < n:
+            if str[r] == '1':
+                t += 1
+                s = max(t, s)
+            
+            elif str[r] == '0':
+                ans = (ans + (s * (s + 1)) // 2) % mod
+                t = 0
+                s = 0
         
-        return ANS
+            r += 1
+        
+        ans = (ans + (s * (s + 1)) // 2) % mod
+        return ans
