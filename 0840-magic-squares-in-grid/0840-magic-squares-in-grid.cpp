b03a259297dce3da936b698df11ac4944b92db84
anuj2001310 <thakuranujbhati310@gmail.com>
2025-12-30 21:13:32 +0530

    Time: 4 ms (10.94%), Space: 12.7 MB (9.42%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
@@ -1,36 +1,46 @@
-#pragma GCC optimize("O2")
-#include <bits/stdc++.h>
-
 class Solution {
-    inline bool isMagic(char i, char j, char a, char b, char c, char d,
-                        vector<vector<int>>& grid) {
-        return (a + grid[i - 1][j] + b == 15) &&
-               (a + grid[i][j - 1] + c == 15) &&
-               (b + grid[i][j + 1] + d == 15) && (c + grid[i + 1][j] + d == 15);
-    }
+    using vi = vector<int>;
+    using vvi = vector<vi>;
 
 public:
     int numMagicSquaresInside(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
-        if (r < 3 || c < 3)
-            return 0;
+        int ans = 0;
 
-        int cnt = 0;
-        for (char i = 1; i < r - 1; i++) {
-            for (char j = 1; j < c - 1; j++) {
-                if (grid[i][j] != 5)
+        vector<vvi> magic = {{{8, 1, 6}, {3, 5, 7}, {4, 9, 2}},
+                             {{6, 1, 8}, {7, 5, 3}, {2, 9, 4}},
+                             {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}},
+                             {{2, 9, 4}, {7, 5, 3}, {6, 1, 8}},
+                             {{8, 3, 4}, {1, 5, 9}, {6, 7, 2}},
+                             {{4, 3, 8}, {9, 5, 1}, {2, 7, 6}},
+                             {{6, 7, 2}, {1, 5, 9}, {8, 3, 4}},
+                             {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}}};
+
+        for (int i = 0; i + 2 < r; i++) {
+            for (int j = 0; j + 2 < c; j++) {
+
+                // Center must be 5
+                if (grid[i + 1][j + 1] != 5)
                     continue;
-                char a = grid[i - 1][j - 1], b = grid[i - 1][j + 1],
-                     c = grid[i + 1][j - 1], d = grid[i + 1][j + 1];
-                uint16_t once = ((1 << 2) | (1 << 4) | (1 << 6) | (1 << 8)) ^
-                                ((1 << a) | (1 << b) | (1 << c) | (1 << d));
-                // check diagonals & center, numbers in corner are even
-                if (once == 0 && (a & 1) == 0 && a + d == 10 && (b & 1) == 0 &&
-                    b + c == 10)
-                    cnt += isMagic(i, j, a, b, c, d, grid);
+
+                for (auto& m : magic) {
+                    bool ok = true;
+                    for (int x = 0; x < 3 && ok; x++) {
+                        for (int y = 0; y < 3; y++) {
+                            if (grid[i + x][j + y] != m[x][y]) {
+                                ok = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (ok) {
+                        ans++;
+                        break;
+                    }
+                }
             }
         }
-        return cnt;
+        return ans;
     }
 };
