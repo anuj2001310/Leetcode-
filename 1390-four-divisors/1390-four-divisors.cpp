d90ba868aa34634c7f6f942065267701fc4afff1
anuj2001310 <thakuranujbhati310@gmail.com>
2026-01-05 00:00:06 +0530

    Time: 0 ms (100%), Space: 17.1 MB (26.85%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,55 +1,26 @@
-#include <vector>
-#include <cmath>
-using namespace std;
+constexpr int MX = 100'001;
+int divisor_num[MX];
+int divisor_sum[MX];
+
+int init = [] {
+    for (int i = 1; i < MX; i++) {
+        for (int j = i; j < MX; j += i) { // 枚举 i 的倍数 j
+            divisor_num[j]++; // i 是 j 的因子
+            divisor_sum[j] += i;
+        }
+    }
+    return 0;
+}();
 
 class Solution {
 public:
-    vector<bool> sieve;
-    
-    void buildSieve(int n) {
-        sieve = vector<bool>(n + 1, true);
-        sieve[0] = sieve[1] = false;
-        for (int i = 2; i * i <= n; i++) {
-            if (sieve[i]) {
-                for (int j = i * i; j <= n; j += i) {
-                    sieve[j] = false;
-                }
-            }
-        }
-    }
-
-    bool isPrime(int x) {
-        if (x < sieve.size()) return sieve[x];
-        return false; // out of bounds numbers (optional)
-    }
-
     int sumFourDivisors(vector<int>& nums) {
-        int totalSum = 0;
-        int maxNum = 0;
-        for (int x : nums) maxNum = max(maxNum, x);
-        
-        buildSieve(maxNum); // precompute primes up to maxNum
-
+        int ans = 0;
         for (int x : nums) {
-            // Case 1: p³
-            int p = round(cbrt(x));
-            if (p * p * p == x && isPrime(p)) {
-                totalSum += (1 + p + p*p + x);
-                continue;
-            }
-
-            // Case 2: p × q
-            for (int i = 2; i * i <= x; i++) {
-                if (x % i == 0) {
-                    int j = x / i;
-                    if (i != j && isPrime(i) && isPrime(j)) {
-                        totalSum += (1 + i + j + x);
-                    }
-                    break; // only first factor needed
-                }
+            if (divisor_num[x] == 4) {
+                ans += divisor_sum[x];
             }
         }
-
-        return totalSum;
+        return ans;
     }
 };
