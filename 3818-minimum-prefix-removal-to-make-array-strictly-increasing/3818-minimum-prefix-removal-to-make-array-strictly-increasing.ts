5ec38004a789079019506e51a2d5539db19037ae
anuj2001310 <thakuranujbhati310@gmail.com>
2026-01-28 17:01:58 +0530

    Time: 1 ms (96.3%), Space: 74.3 MB (96.3%) - LeetHub

---
+++ 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.ts
@@ -0,0 +1,8 @@
+function minimumPrefixLength(nums: number[]): number {
+    let n: number = nums.length;
+    for (let i : number = n - 1; i > 0; i--) {
+        if (nums[i] <= nums[i - 1])
+            return i;
+    }
+    return 0;
+};
