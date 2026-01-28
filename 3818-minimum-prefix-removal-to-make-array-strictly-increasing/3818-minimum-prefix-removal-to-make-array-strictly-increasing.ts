f1db596c53d1f9e8bed6eab1835aa120518d173d
anuj2001310 <thakuranujbhati310@gmail.com>
2026-01-28 17:03:00 +0530

    Time: 2 ms (85.19%), Space: 76 MB (44.44%) - LeetHub

--- 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.ts
+++ 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.ts
@@ -1,6 +1,6 @@
 function minimumPrefixLength(nums: number[]): number {
     let n: number = nums.length;
-    for (let i : number = n - 1; i > 0; i--) {
+    for (let i: number = n - 1; i > 0; i--) {
         if (nums[i] <= nums[i - 1])
             return i;
     }
