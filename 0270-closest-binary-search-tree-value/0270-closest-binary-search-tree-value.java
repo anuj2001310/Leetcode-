6994f2104df3c992535d4d29d10a75af1da00586
anuj2001310 <thakuranujbhati310@gmail.com>
2026-02-10 18:40:59 +0530

    Time: 0 ms (100%), Space: 45.3 MB (10.94%) - LeetHub

---
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    double min = Integer.MAX_VALUE;
+    int result = Integer.MAX_VALUE;
+
+    public int closestValue(TreeNode root, double target) {
+        if (root == null)
+            return result;
+        int val = root.val;
+        double diff = Math.abs(target - (double) val);
+        if (diff < min) {
+            min = diff;
+            result = val;
+        } else if (diff == min) {
+            min = diff;
+            result = Math.min(result, val);
+        }
+        closestValue(root.left, target);
+        closestValue(root.right, target);
+        return result;
+    }
+}
