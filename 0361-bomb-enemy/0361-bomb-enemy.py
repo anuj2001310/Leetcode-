1b8997c67b95be23916a2ccca6a8b59e3f2c9ce1
anuj2001310 <thakuranujbhati310@gmail.com>
2026-02-15 01:44:41 +0530

    Time: 55 ms (100%), Space: 41.5 MB (71.51%) - LeetHub

---
+++ 0361-bomb-enemy/0361-bomb-enemy.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+
+class Solution:
+    def maxKilledEnemies(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dprows = [[0] for _ in range(m)]
+        dpcols = [[0] for _ in range(n)]
+        for i in range(m):
+            for j in range(n):
+
+                if grid[i][j] == "E":
+
+                    dprows[i][-1] += 1
+                    dpcols[j][-1] += 1
+                elif grid[i][j] == "W":
+                    dprows[i].append(0)
+                    dpcols[j].append(0)
+
+        rowidx = [0] * m
+        colidx = [0] * n
+
+        result = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "0":
+                    result = max(result, dprows[i][rowidx[i]] + dpcols[j][colidx[j]])
+
+                elif grid[i][j] == "W":
+                    rowidx[i] += 1
+                    colidx[j] += 1
+
+        return result
