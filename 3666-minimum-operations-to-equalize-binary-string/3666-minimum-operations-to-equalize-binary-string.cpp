bfd0c880b8eee698924bdce4cc32ca86a3089355
anuj2001310 <thakuranujbhati310@gmail.com>
2026-02-28 00:57:41 +0530

    Time: 0 ms (100%), Space: 19.2 MB (84.62%) - LeetHub

--- 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.cpp
+++ 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int minOperations(string s, int k) {
-        int n = s.size(), m = 0;
-        vector<int> dist(n + 1, INT_MAX);
-        vector<set<int>> nodeSets(2);
-        for (int i = 0; i <= n; i++) {
-            nodeSets[i % 2].insert(i);
-            if (i < n && s[i] == '0') {
-                m++;
+        int64_t z = 0 ;
+        for (auto & kk: s) if (kk == '0') ++z ;
+        int64_t n =  s.length(), o = n - z ;
+        int ans = -1 ;
+        if (z == 0) return 0 ;
+        for (int64_t i = 1 ; i <= n ; ++i) {
+            int64_t p = i * (int64_t)k ;
+            if ((p - z) & 1) continue ;
+            if (i & 1) {
+                if (p >= z && p <= (z * i + o * (i - 1)))  return (int)i ;
+            } else {
+                if (p >= z && p <= (z * (i - 1) + o * i)) return (int)i ;
             }
         }
-        queue<int> q;
-        q.push(m);
-        dist[m] = 0;
-        nodeSets[m % 2].erase(m);
-        while (!q.empty()) {
-            m = q.front();
-            q.pop();
-            int c1 = max(k - n + m, 0), c2 = min(m, k);
-            int lnode = m + k - 2 * c2, rnode = m + k - 2 * c1;
-            auto& nodeSet = nodeSets[lnode % 2];
-            for (auto iter = nodeSet.lower_bound(lnode);
-                 iter != nodeSet.end() && *iter <= rnode;) {
-                int m2 = *iter;
-                dist[m2] = dist[m] + 1;
-                q.push(m2);
-                iter = next(iter);
-                nodeSet.erase(m2);
-            }
-        }
-        return dist[0] == INT_MAX ? -1 : dist[0];
+        return ans ;
     }
 };
