0d79903f893473bed2af525f8adb866988e894bc
anuj2001310 <thakuranujbhati310@gmail.com>
2026-03-10 16:03:00 +0530

    Time: 0 ms (100%), Space: 49.4 MB (84.62%) - LeetHub

---
+++ 1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool helper(int idx, int n, TreeNode* root, vector<int>& arr) {
+        if (!root || root->val != arr[idx])
+            return false;
+            
+        if (idx == n - 1)
+            return root->left == nullptr && root->right == nullptr;
+        
+        return helper(idx + 1, n, root->left, arr) || helper(idx + 1, n, root->right, arr);
+    }
+
+public:
+    bool isValidSequence(TreeNode* root, vector<int>& arr) {
+        int n = arr.size();
+        return helper(0, n, root, arr); 
+    }
+};
