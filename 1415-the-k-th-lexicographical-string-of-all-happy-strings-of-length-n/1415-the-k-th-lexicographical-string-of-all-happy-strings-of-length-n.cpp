9b0bc983cb4d24c49a7b022ab3663d70b9c0e605
anuj2001310 <thakuranujbhati310@gmail.com>
2026-03-14 07:59:21 +0530

    Time: 0 ms (100%), Space: 8.1 MB (74.83%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string ans = "";
+    vector<char> v{'a', 'b', 'c'};
+    bool f(int ind, string& s, int& k) {
+        if (ind == s.size()) {
+            k--;
+            if (k == 0) {
+                ans = s;
+                return true;
+            }
+            return false;
+        }
+        for (auto& ch : v) {
+            if (ind == 0 || (ind - 1 >= 0 && s[ind - 1] != ch)) {
+                s[ind] = ch;
+                bool dd = f(ind + 1, s, k);
+                if (dd)
+                    return true;
+                s[ind] = '.';
+            }
+        }
+        return false;
+    }
+
+    string getHappyString(int n, int k) {
+        string s(n, '.');
+        // cout<<s<<endl;
+        f(0, s, k);
+        return ans;
+    }
+};
