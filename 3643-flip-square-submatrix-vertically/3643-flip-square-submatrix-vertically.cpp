1d72129ec7acb53515c92530a7ddf9e1aada5ce9
anuj2001310 <thakuranujbhati310@gmail.com>
2026-03-22 01:26:31 +0530

    Time: 0 ms (100%), Space: 34 MB (30.61%) - LeetHub

--- 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.cpp
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.cpp
@@ -5,13 +5,11 @@ using vvi = vector<vi>;
 
 class Solution {
 public:
-    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x,
-                                         int y, int k) {
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
 
         Rep(r, x, x + k / 2, 1) {
             int minr = x + k - 1 - (r - x);
-            Rep(j, y, y + k, 1) 
-                swap(grid[r][j], grid[minr][j]);
+            Rep(j, y, y + k, 1) swap(grid[r][j], grid[minr][j]);
         }
         return grid;
     }
