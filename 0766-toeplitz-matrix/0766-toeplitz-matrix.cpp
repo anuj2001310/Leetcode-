68d0240dd9a837b4369907fb85c90f6b09f9a80e
anuj2001310 <thakuranujbhati310@gmail.com>
2026-04-12 15:02:19 +0530

    Time: 0 ms (100%), Space: 21 MB (61.95%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    bool isToeplitzMatrix(vector<vector<int>>& mat) {
-        int n = size(mat), m = size(mat[0]);
-        unordered_map<int, unordered_set<int>> map;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++)
-                map[i - j].insert(mat[i][j]);
-        }
-
-        auto it = map.begin();
-        for (; it != map.end(); ++it) {
-            if (size(it->second) != 1)
+    bool calc(vector<vector<int>>& arr, int i, int j, int n, int m) {
+        int data = arr[i][j];
+        i++;
+        j++;
+        while (i < n && j < m) {
+            if (arr[i][j] != data)
                 return false;
+            i++;
+            j++;
+        }
+        return true;
+    }
+
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!calc(matrix, i, j, n, m))
+                    return false;
+            }
         }
         return true;
     }
