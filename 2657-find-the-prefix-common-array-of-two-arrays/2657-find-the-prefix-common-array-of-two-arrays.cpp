5774888c8030a54d7af21aae510d6a8d91a5577c
anuj2001310 <thakuranujbhati310@gmail.com>
2026-05-20 22:50:36 +0530

    Time: 0 ms (100%), Space: 86.1 MB (63.14%) - LeetHub

---
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        ios_base::sync_with_stdio(false);
+        cout.tie(nullptr);
+
+        int n = A.size();
+        vector<bool> presentA(n + 1, false);
+        vector<bool> presentB(n + 1, false);
+        vector<int> ans(n, 0);
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            cnt += presentB[A[i]];
+            cnt += presentA[B[i]];
+            cnt += A[i] == B[i];
+            presentA[A[i]] = true;
+            presentB[B[i]] = true;
+            ans[i] = cnt;
+        }
+        return ans;
+    }
+};
