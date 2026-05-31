8cb12583d16b7167b99e8b3aef085a082d407cff
anuj2001310 <thakuranujbhati310@gmail.com>
2026-05-31 12:10:20 +0530

    Time: 0 ms (100%), Space: 12.4 MB (18.59%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.py
+++ 3945-digit-frequency-score/3945-digit-frequency-score.py
@@ -1,3 +1,12 @@
-class Solution:
-    def digitFrequencyScore(self, n: int) -> int:
-        return sum(int(d) for d in str(n))
+class Solution(object):
+    def digitFrequencyScore(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        s = 0
+        while n:
+            s += n % 10
+            n //= 10
+        
+        return s
