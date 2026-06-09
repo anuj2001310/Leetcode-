e9e35ec67bdd9693382ea49a892dab044d0f36a2
anuj2001310 <thakuranujbhati310@gmail.com>
2026-06-09 20:48:29 +0530

    Time: 0 ms (100%), Space: 57.7 MB (64.86%) - LeetHub

---
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.ts
@@ -0,0 +1,10 @@
+function consecutiveSetBits(n: number): boolean {
+    let cnt: number = 0, p: number = -1;
+    for (; n > 0; n >>= 1) {
+        let r: number = n % 2;
+        if (r == 1 && r == p)
+            cnt++;
+        p = r;
+    }
+    return cnt == 1;
+};
