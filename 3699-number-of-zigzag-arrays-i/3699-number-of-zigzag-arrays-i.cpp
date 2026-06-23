4d1d56252332f5f7d718b72052795e744327e46d
anuj2001310 <thakuranujbhati310@gmail.com>
2026-06-23 13:30:26 +0530

    Time: 204 ms (93.52%), Space: 13.2 MB (66.67%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -1,58 +1,30 @@
 class Solution {
 public:
-    static constexpr int MOD = 1e9 + 7;
-
+    const int mod = 1e9 + 7;
     int zigZagArrays(int n, int l, int r) {
-
-        int m = r - l + 1;
-
-        vector<vector<int>> nxt(m, vector<int>(3, 1));
-        vector<vector<int>> cur(m, vector<int>(3));
-
-        vector<int> pref(m), suff(m);
-
-        for (int pos = n - 1; pos >= 1; pos--) {
-            pref[0] = nxt[0][1];
-            for (int i = 1; i < m; i++) {
-                pref[i] = pref[i - 1] + nxt[i][1];
-                if (pref[i] >= MOD)
-                    pref[i] -= MOD;
+        r = r - l + 1;
+        vector<long long> dp(r, 0);
+        iota(dp.begin(), dp.end(), 0);
+        for (int i = 2; i < n; ++i) {
+            long long pre = 0, cur = 0;
+            if (i % 2 == 0) {
+                for (int j = r - 1; j >= 0; --j) {
+                    cur = (pre + dp[j]) % mod;
+                    dp[j] = pre;
+                    pre = cur;
+                }
+            } else {
+                for (int j = 0; j < r; ++j) {
+                    cur = (pre + dp[j]) % mod;
+                    dp[j] = pre;
+                    pre = cur;
+                }
             }
-
-            suff[m - 1] = nxt[m - 1][2];
-            
-            for (int i = m - 2; i >= 0; i--) {
-                suff[i] = suff[i + 1] + nxt[i][2];
-                if (suff[i] >= MOD)
-                    suff[i] -= MOD;
-            }
-
-            for (int prev = 0; prev < m; prev++) {
-
-                long long left = (prev > 0 ? pref[prev - 1] : 0);
-                long long right = (prev < m - 1 ? suff[prev + 1] : 0);
-
-                // trend = 1
-                cur[prev][0] = (left + right) % MOD;
-
-                // trend = 2
-                cur[prev][1] = (prev == m - 1 ? 0 : suff[prev + 1]);
-
-                // trend = 3
-                cur[prev][2] = (prev == 0 ? 0 : pref[prev - 1]);
-            }
-
-            swap(cur, nxt);
         }
-
         long long ans = 0;
-
-        // choose first element
-        for (int i = 0; i < m; i++) {
-            ans += nxt[i][0];
-            ans %= MOD;
+        for (int i = 0; i < r; ++i) {
+            ans = (ans + dp[i]) % mod;
         }
-
-        return ans;
+        return ans * 2 % mod;
     }
 };
