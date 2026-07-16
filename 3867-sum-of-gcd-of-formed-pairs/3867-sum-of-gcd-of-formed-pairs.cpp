f6ac9b48f7ad37f7b4c54ebd5e269de632c74bcc
anuj2001310 <thakuranujbhati310@gmail.com>
2026-07-16 10:04:52 +0530

    Time: 76 ms (50.08%), Space: 161.6 MB (45.01%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -12,20 +12,15 @@ public:
         Rep(i, 1, n, 1) prefixMax[i] = fmax(prefixMax[i - 1], nums[i]);
 
         vi prefixGcd(n, -1);
-        Rep(i, 0, n, 1) {
-            int val = __gcd(prefixMax[i], nums[i]);
-            prefixGcd[i] = val;
-        }
+        Rep(i, 0, n, 1) prefixGcd[i] = __gcd(prefixMax[i], nums[i]);
+
         Sort(prefixGcd);
         int i = 0, j = n - 1;
         ll sum = 0;
 
-        while (i < j) {
-            int v = __gcd(prefixGcd[i], prefixGcd[j]);
-            sum += v;
-            i++;
-            j--;
-        }
+        while (i < j)
+            sum += __gcd(prefixGcd[i++], prefixGcd[j--]);
+
         return sum;
     }
 };
