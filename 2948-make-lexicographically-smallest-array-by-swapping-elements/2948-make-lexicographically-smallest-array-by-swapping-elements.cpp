51a38deeae6efdc507747bae0e7a91020701d3dd
anuj2001310 <thakuranujbhati310@gmail.com>
2026-08-29 08:59:54 +0530

    Time: 68 ms (100%), Space: 126.8 MB (96.06%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -8,25 +8,20 @@ public:
 
         sort(help.begin(), help.end(),
              [&](const auto& x, const auto& y) { return x.first < y.first; });
-
         vector<int> res(n, 0);
-
         int prev = INT_MIN;
         vector<int> pos;
         for (int s = 0, e = 0; e < n;) {
             pos.push_back(help[e].second);
             prev = help[e].first;
             e++;
-
             if (e == n or help[e].first - prev > limit) {
                 sort(pos.begin(), pos.end());
-                for (auto idx : pos)
+                for (auto& idx : pos)
                     res[idx] = help[s++].first;
-
                 pos.clear();
             }
         }
-
         return res;
     }
 };
