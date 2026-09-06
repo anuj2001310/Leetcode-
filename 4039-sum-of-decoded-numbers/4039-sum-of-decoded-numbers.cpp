59a7e950a753fad0e383942112cc074adcd561d1
anuj2001310 <thakuranujbhati310@gmail.com>
2026-09-06 17:45:41 +0530

    Time: 45 ms (87.5%), Space: 88.9 MB (90.65%) - LeetHub

--- 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.cpp
+++ 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.cpp
@@ -1,45 +1,46 @@
 class Solution {
-    const int MOD = 1000000007;
-    using ll = long long;
-    ll function(ll x, ll y) {
+using ll = long long;
+const int MOD = 1000000007;
+public:
+    ll expo (ll x, ll y) {
         ll res = 1;
         while (y > 0) {
             if ((y & 1))
                 res = (res * x) % MOD;
-
+            
             x = (x * x) % MOD;
             y >>= 1;
         }
-
         return res;
     }
-
-public:
     int sumDecoded(vector<long long>& nums) {
         int n = nums.size();
-        ll res = 0;
 
-        for (int i = 0; i < n; i++) {
+        ll res = 0;
+        for (int i = 0; i < n; ++i) {
             ll num = nums[i];
-            ll width = num % 10;
-            ll d1 = num / 10;
-            ll t = d1;
-            ll dig = 0;
-            for (; t > 0; t /= 10)
+            int w = num % 10;
+            ll d = num / 10;
+            ll t = d;
+            int dig = 0;
+
+            while (t > 0) {
                 dig++;
+                t /= 10;
+            }
 
             ll div = 1;
-            for (ll j = 0; j < dig - width; j++)
+            int rem = dig - w;
+
+            while (rem--)
                 div *= 10;
-
-            ll y = d1 % div;
-            ll x = d1 / div;
-
-            ll power = function(x, y);
+            
+            ll y = d % div;
+            ll x = d / div;
+            ll power = expo(x, y);
 
             res = (res + power) % MOD;
         }
-
         return res;
     }
 };
